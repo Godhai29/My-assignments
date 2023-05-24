15dd5266b49392cfa2c6e42c17003196a9844e99
Godhai29 <131582565+Godhai29@users.noreply.github.com>
2023-05-24 12:27:04 +0530

    Add files via upload
    Week5->Day 1

--- day1/CreateLead.java
+++ day1/CreateLead.java
@@ -1,47 +1,24 @@
-package week2.day1;
+package classroom5.day1;
+
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
 
-public class CreateLead {
+public class CreateLead extends BaseClass {
 
-	public static void main(String[] args) {
-		ChromeDriver driver=new ChromeDriver();
-		
-		driver.manage().window().maximize();
-			driver.get("http://leaftaps.com/opentaps/");
-		    driver.findElement(By.id("username")).sendKeys("demosalesmanager");
-		    driver.findElement(By.id("password")).sendKeys("crmsfa");
-		    driver.findElement(By.className("decorativeSubmit")).click();
-		    driver.findElement(By.partialLinkText("CRM")).click();
-		    driver.findElement(By.linkText("Leads")).click();
-			driver.findElement(By.linkText("Create Lead")).click();
-			driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Test Leaf");
-			driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Godhai");
-			driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Lakshmi");
-			driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Godhai");
-			driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("IT");
-			driver.findElement(By.id("createLeadForm_description")).sendKeys("Software Testing");
-			driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("godhailakshmi2904@gmail.com");
-			WebElement drop1 = driver.findElement(By.id("createLeadForm_dataSourceId"));
-			Select source=new Select(drop1);
-			source.selectByIndex(4);
-			WebElement drop2 = driver.findElement(By.id("createLeadForm_marketingCampaignId"));
-			Select source1=new Select(drop2);
-			source1.selectByVisibleText("Automobile");
-			WebElement drop3 = driver.findElement(By.id("createLeadForm_ownershipEnumId"));
-			Select source2=new Select(drop3);
-			source2.selectByValue("OWN_SCORP");
-			driver.findElement(By.name("submitButton")).click();
-			String text = driver.findElement(By.id("sectionHeaderTitle_leads")).getText();
-		    System.out.println(text);
-		    String title = driver.getTitle();
-		    System.out.println(title);
-		    //driver.close();
-			
-
-	}
-
+@Test
+	public  void runCreateLead () {
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
 }
+}
+
+
+
+
+
+
--- day1/EditLead.java
+++ day1/EditLead.java
@@ -3,20 +3,19 @@ package classroom5.day1;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-
-
-public class DuplicateLead extends BaseClass{
+public class EditLead extends BaseClass {
 @Test
-	public void runDupLead() throws InterruptedException {
-		driver.findElement(By.linkText("Leads")).click();
+	public void runEditLead() throws InterruptedException {
 		driver.findElement(By.linkText("Find Leads")).click();
 		driver.findElement(By.xpath("//span[text()='Phone']")).click();
 		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
-		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("TCS");
 		driver.findElement(By.name("submitButton")).click();
+	
 }
 }
 
